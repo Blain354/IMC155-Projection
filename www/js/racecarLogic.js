deb21ac5c3614efdc58cf24235072e1dbd467a4e
NicoLongfield <lonn1001@usherbrooke.ca>
2022-12-05 16:19:58 -0500

    .

--- www/js/racecarLogic.js
+++ www/js/racecarLogic.js
@@ -4,11 +4,18 @@ var pointsY = [];
 var pointsX = [];
 var AllPoints = [];
 var frontValueRange = 0.045;
-var frontValueThreshold = 0.70;
+var frontValueThreshold = 1.0;
+var minValue = 0.35;
+var sound = document.getElementById("sound");
+sound.volume = 1.0;
+var arrows;
+var blue = "rgb(66, 154, 255)";
+var red = "rgb(255, 59, 59)";
 
 
 //Some initializations after the page has been shown
 $(document).ready(function(){
+    arrows = document.getElementById("containerNextPath");
     root = document.querySelector(':root');
     connectROS('localhost');
 });
@@ -31,7 +38,11 @@ function connectROS(ipAddr) {
             messageType : 'geometry_msgs/Twist'
         });
         listenerSteer.subscribe(function(message) {
-            document.documentElement.style.setProperty(`--angle`,message.angular.z*-50+'deg');
+            if(message.angular.z == 0.0){
+                document.documentElement.style.setProperty(`--angle`,(message.angular.z*-50)+'deg');
+            } else {
+                document.documentElement.style.setProperty(`--angle`,(message.angular.z*-50-4.12)+'deg');
+            }
         });
 
         // Create a topic object to subscribe (SCAN)
@@ -91,8 +102,14 @@ function compute_points(scan) {
 function isProximity(){
     let moyenne = pointsY.reduce((a, b) => a + b, 0) / pointsY.length;
     if(moyenne < frontValueThreshold){
+        sound.playbackRate = 1.8 - Math.abs((moyenne - minValue)/(frontValueThreshold - minValue));
+        sound.play();
+        document.documentElement.style.setProperty(`--back_color`,"red");
         return true;
     } else {
+        sound.pause();
+        sound.currentTime = 0;
+        document.documentElement.style.setProperty(`--back_color`,blue);
         return false;
     }
 }
